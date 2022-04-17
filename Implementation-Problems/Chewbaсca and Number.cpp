818ba895210bb4bdafb2325036bb10c7b7206a98
Mohammad Izhan <43714869+izhan9637@users.noreply.github.com>
2022-04-17 22:48:43 +0530

    Create Chewbaсca and Number.cpp

---
+++ Implementation-Problems/Chewbaсca and Number.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/contest/514/problem/A
+
+# include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string givenNumber;
+    cin >> givenNumber;
+
+    for(int i=0; i<givenNumber.length(); i++){
+        int currentNumber = givenNumber[i] - '0';
+        int invertedNumber = 9 - currentNumber;
+        // If the first digit is 9 then we don't need to change it, as It will create a number with leading zeroes.
+        if(i == 0 && invertedNumber == 1){
+            cout << currentNumber;
+        }else{
+            cout << (invertedNumber < currentNumber ? invertedNumber : currentNumber);
+        }
+    }
+}
